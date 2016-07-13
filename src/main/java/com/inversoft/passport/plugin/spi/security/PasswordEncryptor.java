5f2b2235c749aaa2954001b97bc6082559b8624b
Brian Pontarelli <brian@inversoft.com>
2016-07-12 21:01:26 -0600

    Added factor and new schemes for password encryption and hashing

--- src/main/java/com/inversoft/passport/plugin/spi/security/PasswordEncryptor.java
+++ src/main/java/com/inversoft/passport/plugin/spi/security/PasswordEncryptor.java
@@ -10,11 +10,17 @@ package com.inversoft.passport.plugin.spi.security;
  */
 public interface PasswordEncryptor {
   /**
+   * @return The default factor for this PasswordEncryptor.
+   */
+  int defaultFactor();
+
+  /**
    * Encrypts the given password using the given salt.
    *
    * @param password The password to encrypt.
-   * @param salt The salt that can optionally be used to increase the security of the password encryption.
+   * @param salt     The salt that can optionally be used to increase the security of the password encryption.
+   * @param factor   The load or iteration factor for this encryption operation.
    * @return The encrypted password.
    */
-  String encrypt(String password, String salt);
+  String encrypt(String password, String salt, int factor);
 }
