0fdc7ca6a79e82831fc84860687bca4bfe430d1a
Brian Pontarelli <brian@inversoft.com>
2016-07-18 18:32:19 -0600

    Working

--- src/main/java/com/inversoft/passport/plugin/spi/security/PasswordEncryptor.java
+++ src/main/java/com/inversoft/passport/plugin/spi/security/PasswordEncryptor.java
@@ -3,6 +3,10 @@
  */
 package com.inversoft.passport.plugin.spi.security;
 
+import java.nio.ByteBuffer;
+import java.util.Base64;
+import java.util.UUID;
+
 /**
  * Used to encrypt user passwords.
  *
@@ -24,5 +28,20 @@ public interface PasswordEncryptor {
    */
   String encrypt(String password, String salt, int factor);
 
-
+  /**
+   * Generates a random salt that is compatible with the PasswordEncryptor. The default implementation uses two UUIDs
+   * and Base 64 encodes them.
+   *
+   * @return The salt.
+   */
+  default String generateSalt() {
+    ByteBuffer buf = ByteBuffer.allocate(32);
+    UUID first = UUID.randomUUID();
+    buf.putLong(first.getLeastSignificantBits());
+    buf.putLong(first.getMostSignificantBits());
+    UUID second = UUID.randomUUID();
+    buf.putLong(second.getLeastSignificantBits());
+    buf.putLong(second.getMostSignificantBits());
+    return Base64.getEncoder().encodeToString(buf.array());
+  }
 }
